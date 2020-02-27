239f0411853c4308d17a1c2d2aebaa85e9b0fd27
@lxemily <34338522+LxEmily@users.noreply.github.com>
2020-02-27 15:19:31 +0000

    Implemented admin/loans/create

--- src/main/java/ie/ucd/lms/controller/admin/LoanController.java
+++ src/main/java/ie/ucd/lms/controller/admin/LoanController.java
@@ -1,5 +1,8 @@
 package ie.ucd.lms.controller.admin;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -14,58 +17,114 @@ import ie.ucd.lms.service.admin.LoanHistoryService;
 
 @Controller
 public class LoanController {
-	@Autowired
-	LoanHistoryService loanHistoryService;
+  @Autowired
+  LoanHistoryService loanHistoryService;
 
-	@GetMapping("/admin/loans/view")
-	public String loansView(@RequestParam(defaultValue = "1", required = false) Integer page,
-			@RequestParam(defaultValue = "", required = false) String artifactQuery,
-			@RequestParam(defaultValue = "", required = false) String memberQuery,
-			@RequestParam(defaultValue = "", required = false) String fromDate,
-			@RequestParam(defaultValue = "", required = false) String toDate,
-			@RequestParam(defaultValue = "", required = false) String dateType,
-			@RequestParam(defaultValue = "", required = false) String updateStatus,
-			@RequestParam(defaultValue = "", required = false) String errorMessage, Model model) {
-		Page<LoanHistory> loans = loanHistoryService.searchAllButLost(artifactQuery, memberQuery, fromDate, toDate,
-				dateType, page - 1);
-		model.addAttribute("totalEmptyRows", Common.PAGINATION_ROWS - loans.getTotalElements());
-		model.addAttribute("totalPages", loans.getTotalPages());
-		model.addAttribute("currentPage", page + 1);
-		model.addAttribute("loans", loans);
-		model.addAttribute("daysToRenew", Common.DAYS_TO_RENEW);
+  @GetMapping("/admin/loans/view")
+  public String loansView(@RequestParam(defaultValue = "1", required = false) Integer page,
+      @RequestParam(defaultValue = "", required = false) String artifactQuery,
+      @RequestParam(defaultValue = "", required = false) String memberQuery,
+      @RequestParam(defaultValue = "", required = false) String fromDate,
+      @RequestParam(defaultValue = "", required = false) String toDate,
+      @RequestParam(defaultValue = "", required = false) String dateType,
+      @RequestParam(defaultValue = "", required = false) String updateStatus,
+      @RequestParam(defaultValue = "", required = false) String errorMessage, Model model) {
+    Page<LoanHistory> loans = loanHistoryService.searchAllButLost(artifactQuery, memberQuery, fromDate, toDate,
+        dateType, page - 1);
+    model.addAttribute("totalEmptyRows", Common.PAGINATION_ROWS - loans.getTotalElements());
+    model.addAttribute("totalPages", loans.getTotalPages());
+    model.addAttribute("currentPage", page + 1);
+    model.addAttribute("loans", loans);
+    model.addAttribute("daysToRenew", Common.DAYS_TO_RENEW);
 
-		model.addAttribute("previousArtifact", artifactQuery);
-		model.addAttribute("previousMember", memberQuery);
-		model.addAttribute("previousFromDate", fromDate);
-		model.addAttribute("previousToDate", toDate);
-		model.addAttribute("previousType", dateType);
-		model.addAttribute("previousUpdateStatus", updateStatus);
-		model.addAttribute("previousErrorMessage", errorMessage);
-		return "admin/loan/view.html";
-	}
+    model.addAttribute("previousArtifact", artifactQuery);
+    model.addAttribute("previousMember", memberQuery);
+    model.addAttribute("previousFromDate", fromDate);
+    model.addAttribute("previousToDate", toDate);
+    model.addAttribute("previousType", dateType);
+    model.addAttribute("previousUpdateStatus", updateStatus);
+    model.addAttribute("previousErrorMessage", errorMessage);
+    return "admin/loan/view.html";
+  }
 
-	@PostMapping("/admin/loans/return")
-	@ResponseBody
-	public String loansReturn(@RequestParam(name = "id") String stringId, Model model) {
-		return loanHistoryService.returnn(stringId).toString();
-	}
+  @GetMapping("/admin/loans/create")
+  public String loansCreateGet(Model model) {
+    String returnOn = LocalDate.now().plusDays(7).format(Common.dateFormatter);
+    model.addAttribute("returnOn", returnOn);
+    return "admin/loan/create.html";
+  }
 
-	@PostMapping("/admin/loans/renew")
-	@ResponseBody
-	public String loansRenew(@RequestParam(name = "id") String stringId,
-			@RequestParam(required = false) String daysToRenew, Model model) {
-		return loanHistoryService.renew(stringId, daysToRenew).toString();
-	}
+  @PostMapping("/admin/loans/create")
+  public String loansCreatePost(@RequestParam(defaultValue = "1", required = false) Integer page,
+      @RequestParam(defaultValue = "", required = false) String artifactQuery,
+      @RequestParam(defaultValue = "", required = false) String memberQuery,
+      @RequestParam(defaultValue = "", required = false) String fromDate,
+      @RequestParam(defaultValue = "", required = false) String toDate,
+      @RequestParam(defaultValue = "", required = false) String dateType,
+      @RequestParam(name = "isbn", required = true) String isbn,
+      @RequestParam(name = "title", required = false) String title,
+      @RequestParam(name = "id", required = false) String artifactID,
+      @RequestParam(name = "memberID", required = true) String memberID,
+      @RequestParam(name = "status", defaultValue = "0.00", required = false) String status,
+      @RequestParam(name = "returnOn", required = true) String returnOn,
+      @RequestParam(name = "fine", required = false) String fine,
+      @RequestParam(defaultValue = "", required = false) String updateStatus,
+      @RequestParam(defaultValue = "", required = false) String errorMessage, Model model) {
+    if (loanHistoryService.create(isbn, memberID, returnOn, fine, status)) {
+      Page<LoanHistory> loans = loanHistoryService.searchAllButLost(artifactQuery, memberQuery, fromDate, toDate,
+          dateType, page - 1);
+      model.addAttribute("totalEmptyRows", Common.PAGINATION_ROWS - loans.getTotalElements());
+      model.addAttribute("totalPages", loans.getTotalPages());
+      model.addAttribute("currentPage", page + 1);
+      model.addAttribute("loans", loans);
+      model.addAttribute("daysToRenew", Common.DAYS_TO_RENEW);
 
-	@PostMapping("/admin/loans/lost")
-	@ResponseBody
-	public String loansLost(@RequestParam(name = "id") String stringId, Model model) {
-		return loanHistoryService.lost(stringId).toString();
-	}
+      model.addAttribute("previousArtifact", artifactQuery);
+      model.addAttribute("previousMember", memberQuery);
+      model.addAttribute("previousFromDate", fromDate);
+      model.addAttribute("previousToDate", toDate);
+      model.addAttribute("previousType", dateType);
+      model.addAttribute("previousUpdateStatus", "success");
+      model.addAttribute("previousUpdateMessage", "Created Loan Succesfully.");
+      model.addAttribute("previousErrorMessage", "");
+      return "admin/loan/view.html";
+    } else {
+      model.addAttribute("previousISBN", isbn);
+      model.addAttribute("previousTitle", title);
+      model.addAttribute("previousID", artifactID);
+      model.addAttribute("previousMemberID", memberID);
+      model.addAttribute("previousStatus", status);
+      model.addAttribute("previousReturnOn", returnOn);
+      model.addAttribute("previousFine", fine);
+      model.addAttribute("previousUpdateStatus", "fail");
+      model.addAttribute("previousUpdateMessage", "");
+      model.addAttribute("previousErrorMessage", "Failed to Create Loan. Please try again.");
+      return "admin/loan/create.html";
+    }
+  }
 
-	@PostMapping("/admin/loans/delete")
-	@ResponseBody
-	public String loansDelete(@RequestParam(name = "id") String stringId, Model model) {
-		return loanHistoryService.delete(stringId).toString();
-	}
+  @PostMapping("/admin/loans/return")
+  @ResponseBody
+  public String loansReturn(@RequestParam(name = "id") String stringId, Model model) {
+    return loanHistoryService.returnn(stringId).toString();
+  }
+
+  @PostMapping("/admin/loans/renew")
+  @ResponseBody
+  public String loansRenew(@RequestParam(name = "id") String stringId,
+      @RequestParam(required = false) String daysToRenew, Model model) {
+    return loanHistoryService.renew(stringId, daysToRenew).toString();
+  }
+
+  @PostMapping("/admin/loans/lost")
+  @ResponseBody
+  public String loansLost(@RequestParam(name = "id") String stringId, Model model) {
+    return loanHistoryService.lost(stringId).toString();
+  }
+
+  @PostMapping("/admin/loans/delete")
+  @ResponseBody
+  public String loansDelete(@RequestParam(name = "id") String stringId, Model model) {
+    return loanHistoryService.delete(stringId).toString();
+  }
 }
