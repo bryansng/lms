4e87f5276add356c4803f0f71663266596dc5246
@lxemily <34338522+LxEmily@users.noreply.github.com>
2020-03-03 02:51:37 +0000

    Implemented admin/reserves/edit

--- src/main/java/ie/ucd/lms/controller/ReserveController.java
+++ src/main/java/ie/ucd/lms/controller/ReserveController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
+
+import ie.ucd.lms.dao.ReserveQueueRepository;
 import ie.ucd.lms.entity.ReserveQueue;
 import ie.ucd.lms.service.ActionConclusion;
 import ie.ucd.lms.service.Common;
@@ -20,6 +22,8 @@ import ie.ucd.lms.service.ReserveQueueService;
 public class ReserveController {
   @Autowired
   ReserveQueueService reserveQueueService;
+  @Autowired
+  ReserveQueueRepository reserveQueueRepository;
 
   @GetMapping("/admin/reserves/view")
   public String reservesView(@RequestParam(defaultValue = "1", required = false) Integer page,
@@ -55,6 +59,14 @@ public class ReserveController {
   public String reservesCreateGet(Model model) {
     model.addAttribute("expiredOn", LocalDate.now().plusDays(7).format(Common.dateFormatter));
     return "admin/reserve/create.html";
+  }
+
+  @GetMapping("/admin/reserves/edit")
+  public String reservesEditGet(@RequestParam(name = "id") String stringId, Model model) {
+    ReserveQueue reserve = reserveQueueRepository.findById(Common.convertStringToLong(stringId)).get();
+    model.addAttribute("reserve", reserve);
+    model.addAttribute("expiredOn", reserve.getExpiredOn().format(Common.dateFormatter));
+    return "admin/reserve/edit.html";
   }
 
   @PostMapping("/admin/reserves/create")
@@ -99,10 +111,56 @@ public class ReserveController {
       model.addAttribute("previousTitle", title);
       model.addAttribute("previousID", artifactID);
       model.addAttribute("previousMemberID", memberID);
-      model.addAttribute("previousStatus", status);
       model.addAttribute("previousExpiredOn", expiredOn);
-      model.addAttribute("previousFine", fine);
       return "admin/reserve/create.html";
+    }
+  }
+
+  @PostMapping("/admin/reserves/edit")
+  public String reservesEditPost(@RequestParam(name = "id") String stringId,
+      @RequestParam(defaultValue = "1", required = false) Integer page,
+      @RequestParam(defaultValue = "", required = false) String artifactQuery,
+      @RequestParam(defaultValue = "", required = false) String memberQuery,
+      @RequestParam(defaultValue = "", required = false) String fromDate,
+      @RequestParam(defaultValue = "", required = false) String toDate,
+      @RequestParam(defaultValue = "", required = false) String dateType,
+      @RequestParam(name = "isbn", required = true) String isbn,
+      @RequestParam(name = "title", defaultValue = "", required = false) String title,
+      @RequestParam(name = "artifactID", required = false) String artifactID,
+      @RequestParam(name = "memberID", required = true) String memberID,
+      @RequestParam(name = "status", defaultValue = "", required = false) String status,
+      @RequestParam(name = "expiredOn", required = true) String expiredOn,
+      @RequestParam(name = "fine", defaultValue = "0.00", required = false) String fine,
+      @RequestParam(defaultValue = "", required = false) String isSuccess,
+      @RequestParam(defaultValue = "", required = false) String successMessage,
+      @RequestParam(defaultValue = "", required = false) String failureMessage, Model model) {
+    ActionConclusion actionConclusion = reserveQueueService.update(stringId, isbn, memberID, expiredOn);
+    model.addAttribute("previousIsSuccess", actionConclusion.isSuccess.toString());
+    model.addAttribute("previousSuccessMessage", actionConclusion.message);
+    model.addAttribute("previousFailureMessage", actionConclusion.message);
+    if (actionConclusion.isSuccess) {
+      Page<ReserveQueue> reserves = reserveQueueService.search(artifactQuery, memberQuery, fromDate, toDate, page - 1);
+      Map<Long, Long> positionInQueue = reserveQueueService.searchFirstInQueueByArtifact(artifactQuery, memberQuery,
+          fromDate, toDate, page - 1);
+      model.addAttribute("totalEmptyRows", Common.PAGINATION_ROWS - reserves.getTotalElements());
+      model.addAttribute("totalPages", reserves.getTotalPages());
+      model.addAttribute("currentPage", page);
+      model.addAttribute("reserves", reserves);
+      model.addAttribute("positionInQueue", positionInQueue);
+      model.addAttribute("daysToLoan", Common.DAYS_TO_LOAN);
+
+      model.addAttribute("previousArtifact", artifactQuery);
+      model.addAttribute("previousMember", memberQuery);
+      model.addAttribute("previousFromDate", fromDate);
+      model.addAttribute("previousToDate", toDate);
+      return "admin/reserve/view.html";
+    } else {
+      model.addAttribute("previousISBN", isbn);
+      model.addAttribute("previousTitle", title);
+      model.addAttribute("previousID", artifactID);
+      model.addAttribute("previousMemberID", memberID);
+      model.addAttribute("previousExpiredOn", expiredOn);
+      return "admin/reserve/edit.html";
     }
   }
 
